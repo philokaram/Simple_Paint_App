948781af1cff39ca4900fa755bbd81cb3c1b1f29
philokaram <philokrm@gmail.com>
2025-05-26 15:40:22 +0200

    fix dda

--- Project/Algorithms/line/DDA.cpp
+++ Project/Algorithms/line/DDA.cpp
@@ -5,10 +5,19 @@ int Round(double x)
 }
 void DrawLineDDA(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c)
 {
+	
 	int dx = x2 - x1, dy = y2 - y1;
 	SetPixel(hdc, x1, y1, c);
 	if (abs(dx) >= abs(dy))
 	{
+		if(x2 < x1){
+			int tempx = x1;
+			int tempy = y1;
+			x1 = x2;
+			y1 = y2;
+			x2 =  tempx;
+			y2 =  tempy;
+    	}
 		double m = (double)dy / dx;
 		int x = x1;
 		double y = y1;
@@ -20,6 +29,14 @@ void DrawLineDDA(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c)
 		}
 	}
 	else {
+		if(y2 < y1){
+			int tempx = x1;
+			int tempy = y1;
+			x1 = x2;
+			y1 = y2;
+			x2 =  tempx;
+			y2 =  tempy;
+    	}
 		double mi = (double)dx / dy;
 		int y = y1;
 		double x = x1;
